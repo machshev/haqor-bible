49979fbe3f26c41163335de87b98305bbf027048
David James McCorrie <djmccorrie@gmail.com>
2023-10-29 13:43:52 +0000

    default library path from environment variable or

--- haqor-engine/src/lib.rs
+++ haqor-engine/src/lib.rs
@@ -1,64 +1,75 @@
 // Load text from bible verse (SQLite)
 
-use std::path::{PathBuf, Path};
+use dirs;
+use std::env;
+use std::path::{Path, PathBuf};
 
+#[derive(Debug)]
 pub struct Bible {
     pub name: String,
-    file_name: String,
+    file_path: PathBuf,
 }
 
-
+#[derive(Debug)]
 pub struct Library {
     base_path: PathBuf,
 }
 
 impl Default for Library {
     fn default() -> Self {
-        Library {base_path: PathBuf::from("~/.haqor/library/")}
+        let base_path: PathBuf = match env::var("HAQOR_LIBRARY_PATH") {
+            Ok(i) => PathBuf::from(i),
+            _ => dirs::data_dir()
+                .expect("Can't access data dir")
+                .join("haqor/library"),
+        };
+
+        Library { base_path }
     }
 }
 
-
 impl Library {
-
-    pub fn get_library(base_path: &Path) -> Library{
-        Library {base_path: base_path.to_path_buf()}
+    pub fn get_library(base_path: &Path) -> Library {
+        Library {
+            base_path: base_path.to_path_buf(),
+        }
     }
 
     pub fn get_bible(&self, name: &str) -> Bible {
-        let file_name: String = "file_path".into();
+        let name: String = name.to_string();
+        let file_path: PathBuf = self.base_path.join(format!("{}.bbl.mybible.gz", name));
 
-        log::info!("Loading bible '{}'", name);
-        
-        Bible {name: "Test Bible".to_string(), file_name}
+        log::info!(
+            "Loading bible '{}' from '{:?}' exists '{}'",
+            name,
+            file_path,
+            file_path.exists()
+        );
+
+        Bible { name, file_path }
     }
-
 }
-
-
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn test_get_default_library(){
+    fn test_get_default_library() {
         let lib = Library::default();
 
         assert_eq!(lib.base_path, Path::new("~/.haqor/library/"));
     }
 
     #[test]
-    fn test_get_library(){
+    fn test_get_library() {
         let lib = Library::get_library(Path::new("../test_library"));
 
         assert_eq!(lib.base_path, Path::new("../test_library"));
     }
 
-
-    
     #[test]
-    fn test_get_bible(){
+    fn test_get_bible() {
         let lib = Library::default();
         let bible = lib.get_bible("test_bible");
 
--- haqor-engine/src/main.rs
+++ haqor-engine/src/main.rs
@@ -1,5 +1,6 @@
 use clap::Parser;
-use std::path::PathBuf;
+use haqor_engine::{Bible, Library};
+use log::info;
 
 /// Summarise bible resource
 #[derive(Parser)]
@@ -8,18 +9,22 @@ use std::path::PathBuf;
 #[command(version = "0.1")]
 #[command(
     about = "CLI for haqor",
-    long_about = "This tool is mostly for testing purposes. It allows basic operations with the backend rust based engine. It's not expected to have utility beyond that at this stage."
+    long_about = "This tool is mostly for testing purposes. It allows basic 
+    operations with the backend rust based engine. It's not expected to have 
+    utility beyond that at this stage."
 )]
 struct Args {
     /// The bible to load
     bible: String,
-    /// Where to find the bible
-    path: PathBuf,
 }
 
 fn main() {
+    env_logger::init();
+
     let args = Args::parse();
 
-    println!("Bible: {}", args.bible);
-    println!("Path: {}", args.path.display());
+    let library = Library::default();
+    let bible: Bible = library.get_bible(&*args.bible);
+
+    info!("Bible: {:?}", bible);
 }
