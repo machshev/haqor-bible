d0e5799425790630925d730a0bda6511b031efb2
David James McCorrie <djmccorrie@gmail.com>
2023-12-17 22:45:19 +0000

    Set log level via clap verbose arg

--- haqor-engine/src/lib.rs
+++ haqor-engine/src/lib.rs
@@ -1,13 +1,41 @@
 // Load text from bible verse (SQLite)
 
-use dirs;
 use std::env;
+use std::fs::File;
+use std::io;
+use std::os::unix::ffi::OsStrExt;
 use std::path::{Path, PathBuf};
 
 #[derive(Debug)]
 pub struct Bible {
     pub name: String,
     file_path: PathBuf,
+}
+
+#[derive(Debug)]
+pub struct ResourceRepo {
+    pub name: String,
+    url: String,
+}
+
+impl Default for ResourceRepo {
+    fn default() -> Self {
+        ResourceRepo {
+            name: "mysword".to_string(),
+            url: "https://mysword-bible.info/download/getfile.php?file=".to_string(),
+        }
+    }
+}
+
+impl ResourceRepo {
+    fn fetch_bible(&self, library: Library, name: &str) {
+        let bible_url = format!("{}{}.bbl.mybible.gz", self.url, name);
+
+        let resp = reqwest::blocking::get(bible_url).expect("request failed");
+        let body = resp.text().expect("body invalid");
+
+        library.save_bible(name, &body);
+    }
 }
 
 #[derive(Debug)]
@@ -35,6 +63,11 @@ impl Library {
         }
     }
 
+    pub fn save_bible(&self, file_name: &str, content: &str) {
+        let mut out = File::create(self.base_path.join(file_name)).expect("failed to create file");
+        io::copy(&mut content.as_bytes(), &mut out).expect("failed to copy content");
+    }
+
     pub fn get_bible(&self, name: &str) -> Bible {
         let name: String = name.to_string();
         let file_path: PathBuf = self.base_path.join(format!("{}.bbl.mybible.gz", name));
@@ -58,7 +91,12 @@ mod tests {
     fn test_get_default_library() {
         let lib = Library::default();
 
-        assert_eq!(lib.base_path, Path::new("~/.haqor/library/"));
+        assert_eq!(
+            lib.base_path,
+            dirs::data_dir()
+                .expect("Can't access data dir")
+                .join("haqor/library/")
+        );
     }
 
     #[test]
@@ -69,12 +107,36 @@ mod tests {
     }
 
     #[test]
+    fn test_default_resource_repo() {
+        let repo = ResourceRepo::default();
+
+        assert_eq!(repo.name, "mysword");
+        assert_eq!(
+            repo.url,
+            "https://mysword-bible.info/download/getfile.php?file="
+        );
+    }
+
+    #[test]
+    fn test_resource_repo_fetch_bible() {
+        let repo = ResourceRepo::default();
+        let lib = Library::default();
+
+        repo.fetch_bible(lib, "kjv");
+    }
+
+    #[test]
     fn test_get_bible() {
         let lib = Library::default();
         let bible = lib.get_bible("test_bible");
 
-        assert_eq!(bible.name, "Test Bible");
-        assert_eq!(bible.file_name, "file_path");
+        assert_eq!(bible.name, "test_bible");
+        assert_eq!(
+            bible.file_path,
+            dirs::data_dir()
+                .expect("Can't access data dir")
+                .join("haqor/library/test_bible.bbl.mybible.gz")
+        );
     }
 
     /*
--- haqor-engine/src/main.rs
+++ haqor-engine/src/main.rs
@@ -1,9 +1,10 @@
-use clap::Parser;
-use haqor_engine::{Bible, Library};
-use log::info;
+use clap::{Parser, Subcommand};
+//use haqor_engine::{Bible, Library};
+use log::{debug, error, info, trace, warn};
+use std::env;
 
 /// Summarise bible resource
-#[derive(Parser)]
+#[derive(Parser, Debug)]
 #[command(name = "haqor-engine")]
 #[command(author = "James McCorrie <djmccorrie@gmail.com>")]
 #[command(version = "0.1")]
@@ -13,18 +14,47 @@ use log::info;
     operations with the backend rust based engine. It's not expected to have 
     utility beyond that at this stage."
 )]
-struct Args {
-    /// The bible to load
-    bible: String,
+struct Cli {
+    #[arg(short, long, action = clap::ArgAction::Count)]
+    verbose: u8,
+
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand, Debug)]
+enum Commands {
+    /// Fetch bible from remote repository
+    Fetch {
+        /// Name of the bible to download
+        bible: String,
+    },
 }
 
 fn main() {
+    let cli = Cli::parse();
+
+    if env::var("RUST_LOG").is_err() {
+        env::set_var(
+            "RUST_LOG",
+            match cli.verbose {
+                0 => "Error",
+                1 => "Info",
+                2 => "Debug",
+                _ => "Trace",
+            },
+        );
+    }
     env_logger::init();
 
-    let args = Args::parse();
+    error!("Error");
+    warn!("Warn");
+    info!("Info");
+    debug!("Debug");
+    trace!("Trace");
 
-    let library = Library::default();
-    let bible: Bible = library.get_bible(&*args.bible);
+    // let library = Library::default();
+    // let bible: Bible = library.get_bible(&*cli.bible);
 
-    info!("Bible: {:?}", bible);
+    // info!("Bible: {:?}", bible);
 }
